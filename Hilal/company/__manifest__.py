bf4f8406b5588329a0d407c739ac07970a5ad094
example <you@example.com>
2020-01-17 16:05:27 +0300

    Hilal company application

---
+++ Hilal/company/__manifest__.py
@@ -0,0 +1,13 @@
+{'name': 'Company Application',
+ 'description': 'Company',
+ 'author': 'Hilal Kaya',
+ 'depends': ['base', 'hr'],
+ 'data': [
+
+     'views/company.xml',
+     'views/company_view.xml',
+
+ ],
+ 'application': True,
+ 'installable': True,
+ }
